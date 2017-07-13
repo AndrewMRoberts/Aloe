b68e6ede84e22d562e0c849304fe31eeb5879785
Andrew Roberts <andrew.mike.roberts@gmail.com>
2017-07-12 20:26:03 -0500

    Initial commit of example project. Prints JSON to browser

---
+++ src/api/Controllers/ContactsController.cs
@@ -0,0 +1,20 @@
+using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace api.Controllers
+{
+    [Route("api/[controller]")]
+    public class ContactsController : Controller
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            var result = new [] {
+                new { FirstName = "Jimmy", LastName = "Bob"},
+                new { FirstName = "Mike", LastName = "Krcynzski"}
+            };
+
+            return Ok(result);
+        }
+    }
+}
---
+++ src/api/Main.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace api
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseStartup<Startup>()
+                .Build();
+
+            host.Run();
+        }
+    }
+
+    public class Startup
+    {
+        public Startup(IHostingEnvironment env) {
+
+        }
+
+        // This method is called by the runtime. Use this method to add services
+        // to the container
+        public void ConfigureServices(IServiceCollection services) {
+            services.AddMvc().AddJsonOptions(options => {
+                options.SerializerSettings.ContractResolver = new Newtonsoft.Json.Serialization.DefaultContractResolver();
+            });
+        }
+
+        // This method is called by the runtime. Use this method to configure
+        // the HTTP request pipeline
+        public void Configure(IApplicationBuilder app) {
+            app.UseMvc();
+        }
+    }
+}
