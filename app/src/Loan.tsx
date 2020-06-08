1d6e18ee7d4ebd5ef43813f85eb54bf76197c584
Andrew Roberts <andrew.mike.roberts@gmail.com>
2020-06-07 22:01:15 -0500

    Restart _again_

---
+++ app/src/Loan.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+import './Loan.css';
+
+import Line from 'react-chartjs-2';
+
+function Loan() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default Loan;
---
+++ app/src/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import './index.css';
+
+import Loan from './Loan';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Loan/>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
